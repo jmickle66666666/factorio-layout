c75ee24fe270e976b90ea92e0dd30f5556087bf5
jmickle66666666 <jaymickle@gmail.com>
2017-06-27 18:25:20 +0100

    ups

--- recipe.js
+++ recipe.js
@@ -1,38 +1,43 @@
 function getRecipe(result) {
-
-    if (data.recipe[result] != null) {
-        if (data.recipe[result].result == result) {
-            return data.recipe[result]; // found a direct recipe
+    if (data.recipe[result] != null && data.recipe[result].normal != null) {
+        if (data.recipe[result].normal.result == result) {
+            return data.recipe[result].normal; // found a direct recipe
         } else {
-            if (data.recipe[result].results != null) {
-                for (var i = 0; i < data.recipe[result].results.length; i++) {
-                    if (data.recipe[result].results[0].name == result) {
-                        return data.recipe[result];
+            if (data.recipe[result].normal.results != null) {
+                for (var i = 0; i < data.recipe[result].normal.results.length; i++) {
+                    if (data.recipe[result].normal.results[0].name == result) {
+                        return data.recipe[result].normal;
                     }
                 }
             }
         }
     } else {
         // ok iterate the entire thing, sorry
-
         var output = [];
 
-        for each (recipe in data.recipe) {
-            if (recipe.result == result) {
-                output.push(recipe.result);
-            } else {
-                if (recipe.results != null) {
-                    for (var i = 0; i < data.recipe[result].results.length; i++) {
-                        if (recipe.results[0].name == result) {
+        for (var recipe in data.recipe) {
+            if (data.recipe.hasOwnProperty(recipe)) {
+                recipe = data.recipe[recipe];
+                
+                if (recipe.normal != null) recipe = recipe.normal;
+
+                if (recipe.result != null) {
+                    if (recipe.result == recipe) output.push(recipe);
+                } else if (recipe.results != null) {
+                    for (var r in recipe.results) {
+                        r = recipe.results[r];
+                        if (r.name == result) {
                             output.push(recipe);
+                            break;
                         }
                     }
-                }
             }
-        }
-    }
+            }
 
-    if (output.length == 0) return null;
-    if (output.length == 1) return output[0];
-    return output;
+        }
+        if (output.length == 0) return null;
+        if (output.length == 1) return output[0];
+        return output;
+    }
+    
 }
